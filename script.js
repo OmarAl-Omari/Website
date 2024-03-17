c1e0c58cb0e0dfc6fdb1f0b8588d69ff4c7a5dbb
OmarAl-Omari <omaralomarijor@gmail.com>
2024-03-17 15:37:26 -0700

    ss

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 let lastScrollTop = 0;
 const header = document.getElementById("header-js");
-const threshold = 60;
 
 window.addEventListener("scroll", (event) => {
     const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-    const scrollDifference = currentScroll - lastScrollTop;
 
-    if (currentScroll > lastScrollTop) {
-        // Scrolling down
-        header.style.transform = "translateY(-100%)";
-    } else if (currentScroll===0) {
+    if (currentScroll===0) {
         // Scrolling up
         header.style.transform = "translateY(0)";
+    }
+    else{
+        header.style.transform = "translateY(-100%)";
     }
     
     lastScrollTop = currentScroll;
